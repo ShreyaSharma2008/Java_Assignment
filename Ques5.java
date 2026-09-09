f7132daddd500bc5d1184195164d3d5968b33c8c
Shreya Sharma <shreyasharma3075@gmail.com>
2026-09-09 22:34:35 +0530

    Create Ques5.java

---
+++ Ques5.java
@@ -0,0 +1,9 @@
+class Ques5{
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < args.length; i++) {
+            sum = sum + Integer.parseInt(args[i]);
+        }
+        System.out.println("Sum of command line arguments = " + sum);
+    }
+}
